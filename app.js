6ef2ae64a53d2fbad10fcb727df8be48e931ed58
aakashsr <aakashsri.dev@gmail.com>
2020-04-05 12:42:22 +0530

    added a function to run when game initializes and also added a state variable to check if game over or not

--- app.js
+++ app.js
@@ -83,3 +83,27 @@ function togglePlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
 }
+
+function init() {
+  // Hide dice initially
+  dice.style.display = "none";
+
+  score = [0, 0];
+  roundScore = 0;
+  currentPlayer = 0;
+  // State variable
+  isPlaying = true;
+
+  // Setting initial score to 0
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.querySelector("#name-0").textContent = "Player 0";
+  document.querySelector("#name-1").textContent = "Player 1";
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
